d242817c28eeaaf7ca9d404a596c3839bbe1fb34
Neil McCallion <neil@rehabstudio.com>
2013-09-09 13:27:20 +0100

    Added Jasmine unit test framework and skeleton for Proto 1

---
+++ static/js/tests/spec/Proto1.js
@@ -0,0 +1,115 @@
+describe('The game', function() {
+
+		it('has at least 2 players', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('has a generated map', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+});
+
+describe('The game map', function() {
+
+		it('is square', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		if('is created from a given seed', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('is generated identically every time for the same seed', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('is populated prior to the start of the game', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('is identical for all players', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('has no user-created walls on it at game start', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		describe('when it is clicked', function() {
+
+				it('a new wall should be built if the clicked cell is empty', function() {
+
+						expect(false).toBe(true);
+
+				});
+
+				it('all players should see the newly-built wall', function() {
+
+						expect(false).toBe(true);
+
+				});
+
+		});
+
+});
+
+describe('A grid cell', function() {
+
+
+		it('has a terrain type', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('can have a wall placed on it if empty', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('prevents building on it if not empty', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+		it('can be cleared and made empty again', function() {
+
+				expect(false).toBe(true);
+
+		});
+
+});
+
+describe('Commands', function() {
+
+
+		describe('After the "clear" command is sent', function() {
+
+				it('no user-created walls are left on the map', function() {
+
+						expect(false).toBe(true);
+
+				});
+
+		});
+
+});
