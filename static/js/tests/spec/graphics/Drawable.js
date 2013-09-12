8c5029f6acf72615b49621d4de825e2e7445331b
Ryan Grieve <ryan@rehabstudio.com>
2013-09-12 13:47:17 +0100

    comitting all to be sure to be sure

--- static/js/tests/spec/graphics/Drawable.js
+++ static/js/tests/spec/graphics/Drawable.js
@@ -1,22 +1,26 @@
 define(
     [],
     function(){
-        return function(DrawableObject){
+        return function(){
             describe(
                 "behaving like Drawable", 
                 function(){
-                    var drawableObject = new DrawableObject();
                     it("should have a callable Render() function", function(){
-                        expect(drawableObject.render).toBeDefined();
-                        expect(drawableObject.render instanceof Function).toBeTruthy();
+                        runs(function(){
+                            expect(this.env.instance.render).toBeDefined();
+                            expect(this.env.instance.render instanceof Function).toBeTruthy();
+                        });
                     });
                     it("should return a drawable object when Render() is called", function(){
-                        spyOn(drawableObject, 'render').andCallThrough();
-                        var returnValue = drawableObject.render();
-                        expect(drawableObject.render).toHaveBeenCalled();
-                        var isImage = returnValue instanceof Image;
-                        var isCanvas = returnValue instanceof HTMLElement && returnValue.nodeName.toLowerCase() === "canvas";
-                        expect(isImage || isCanvas).toBeTruthy();
+                        runs(function(){
+                            console.log(this);
+                            spyOn(this.env.instance, 'render').andCallThrough();
+                            var returnValue = this.env.instance.render();
+                            expect(this.env.instance.render).toHaveBeenCalled();
+                            var isImage = returnValue instanceof Image;
+                            var isCanvas = returnValue instanceof HTMLElement && returnValue.nodeName.toLowerCase() === "canvas";
+                            expect(isImage || isCanvas).toBeTruthy();
+                        });
                     });
                 }
             ); 
